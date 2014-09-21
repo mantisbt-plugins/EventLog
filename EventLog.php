d4174c2d4d79d5c6d432c5b0d2edbd5f9e9fd7f5
Victor Boctor <victor@mantishub.net>
2014-09-20 23:28:49 -0700

    Update dependency to MantisCore 1.3
    - Update dependency
    - Update contact details
    - Update version number

--- EventLog.php
+++ EventLog.php
@@ -32,13 +32,13 @@ class EventLogPlugin extends MantisPlugin {
 		$this->name		= plugin_lang_get( 'title' );
 		$this->description	= plugin_lang_get( 'description' );
 
-		$this->version		= '1.0';
+		$this->version		= '1.1';
 		$this->requires		= array(
-			'MantisCore' => '1.2.0',
+			'MantisCore' => '1.3.0',
 		);
 
 		$this->author		= 'Victor Boctor';
-		$this->contact		= 'victor@mantishub.com';
+		$this->contact		= 'victor@mantishub.net';
 		$this->url			= 'http://www.mantishub.com';
 	}
 
