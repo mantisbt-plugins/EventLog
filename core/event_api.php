70a88dc07486ebb2d66cab64380ceb5fa40e27d8
Victor Boctor <victor@mantishub.net>
2015-12-19 22:13:05 -0800

    Fix events clear page

--- core/event_api.php
+++ core/event_api.php
@@ -76,8 +76,9 @@ function event_get_by_request_id( $p_request_id ) {
 
 /**
  * Clears the event log.
+ * @return void
  */
-function event_clear() {
+function event_clear_all() {
 	$t_events_table = plugin_table( 'events' );
 
 	$t_query = "DELETE FROM $t_events_table";
